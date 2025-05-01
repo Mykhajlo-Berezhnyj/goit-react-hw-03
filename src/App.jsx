6501ee76d752840110f2892a38caf4fe00113a31
Mykhajlo-Berezhnyj <bmve@ukr.net>
2025-05-01 05:41:42 +0300

    add style & fix delete & search

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 import initionalContacts from './contactList.json';
 import ContactList from './ContactList/ContactList';
 import SearchBox from './SearchBox/SearchBox';
 import ContactForm from './ContactForm/ContactForm';
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css';
 import './App.css';
 
 function App() {
@@ -15,32 +17,51 @@ function App() {
   });
   useEffect(() => {
     window.localStorage.setItem('saved-phonebook', JSON.stringify(contacts));
-}, [contacts])
+  }, [contacts]);
 
   const [filter, setFilter] = useState('');
 
   const normalize = str => str.replace(/\D/g, '');
-  const isNumeric = Number.isFinite(Number(filter.trim()));
-  const filteredContacts = contacts.filter((contact) => contact.name.toLowerCase().includes(filter.toLowerCase()) || ( isNumeric && normalize(contact.number).includes(normalize(filter))));
+  const isNumeric = Number.isFinite(Number(normalize(filter)));
+  const filteredContacts = contacts.filter(
+    contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase()) ||
+      (isNumeric && normalize(contact.number).includes(normalize(filter))),
+  );
 
-  const addContact = (newContact) => {
-    setContacts((prev) => {
-      return [...prev, newContact,];
+  const addContact = newContact => {
+    setContacts(prev => {
+      return [...prev, newContact];
     });
   };
 
-  const deleteContact = (deleteId) => {
-    setContacts((prev) => prev.filter(contact => contact.id !== deleteId));
+  const deleteContact = deleteId => {
+    confirmAlert({
+      title: 'Confirm Delete',
+      message: 'You confirm delete this contact?',
+      buttons: [
+        {
+          label: 'Yes',
+          onClick: () =>
+            setContacts(prev =>
+              prev.filter(contact => contact.id !== deleteId),
+            ),
+        },
+        {
+          label: 'No',
+        },
+      ],
+    });
   };
 
   return (
-    <div>
+    <div className="container">
       <h1>Phonebook</h1>
       <ContactForm onContact={addContact} />
-      <SearchBox value={filter} onFilterChange={setFilter} /> 
+      <SearchBox value={filter} onFilterChange={setFilter} />
       <ContactList contacts={filteredContacts} onDelete={deleteContact} />
     </div>
-  )
+  );
 }
 
 export default App;
